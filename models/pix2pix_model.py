416037b67ed87a22d65ee92e4d145d303c60942d
Teliov Croby <obinna@prowork.me>
2019-07-07 09:17:38 +0200

    fixed spelling bug with cosine similarity loss

--- models/pix2pix_model.py
+++ models/pix2pix_model.py
@@ -115,9 +115,9 @@ class Pix2PixModel(BaseModel):
         self.loss_G_GAN = self.criterionGAN(pred_fake, True)
         # Second, G(A) = B
         # self.loss_G_L1 = self.criterionL1(self.fake_B, self.real_B) * self.opt.lambda_L1
-        self.loss_G_cosine = self.compute_cosine_distance(self.fake_B, self.real_B) * self.opt.lambda_Cosine
+        self.loss_G_Cosine = self.compute_cosine_distance(self.fake_B, self.real_B) * self.opt.lambda_Cosine
         # combine loss and calculate gradients
-        self.loss_G = self.loss_G_GAN + self.loss_G_cosine
+        self.loss_G = self.loss_G_GAN + self.loss_G_Cosine
         self.loss_G.backward()
 
     def optimize_parameters(self):
